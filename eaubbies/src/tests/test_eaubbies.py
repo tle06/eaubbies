3ee899cf09e7c470b58bee6433b203221a9a32bf
tle06 <tle@tlnk.fr>
2026-06-08 08:56:21 +0200

    feat: fix linting

--- eaubbies/src/tests/test_eaubbies.py
+++ eaubbies/src/tests/test_eaubbies.py
@@ -1,14 +1,11 @@
 import sys
 import os
 import pytest
-
-# Add eaubbies/src to sys.path so we can import utils and modules directly
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-)
-
 from utils.tesseract_client import TesseractClient
 from utils.utils import volume_converter, generate_result
+
+# Add eaubbies/src to sys.path so we can import utils and modules directly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def test_volume_converter():
