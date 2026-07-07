b14f3e3e5f23bbeb44aba2bf4f0a73a95a0a4ea7
tle06 <tle@tlnk.fr>
2026-07-07 17:06:57 +0200

    feat: add default port

--- eaubbies/entrypoint/entrypoint.d/0.sh
+++ eaubbies/entrypoint/entrypoint.d/0.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-echo "[EXCUTION] 0.sh file"
+echo "[EXECUTION] 0.sh file"
+
+# If PORT is unset or empty, default to 8099
+export PORT=${PORT:-8099}
+echo "[INFO] Target port is set to: $PORT"
 
 start_web() {
-
     echo "-------------------------------------"
     echo "[PREPARATION] Create NGINX configuration file at /etc/nginx/sites-available/app.conf"
     echo "NGINX will use the PORT: $PORT"
     envsubst "\$PORT" < /etc/nginx/conf.d/app.template > /etc/nginx/sites-available/app.conf
 
     echo "[PREPARATION] Activate NGINX configuration file"
-    ln -s /etc/nginx/sites-available/app.conf /etc/nginx/sites-enabled/
+    # Added -f (force) to prevent errors if the symlink already exists on container restart
+    ln -sf /etc/nginx/sites-available/app.conf /etc/nginx/sites-enabled/
+    
     echo "[PREPARATION] Finished"
     echo "Server will start now with the following command:"
     echo "/usr/bin/supervisord -c /etc/supervisor/supervisord.conf"
     echo "-------------------------------------"
     /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-
 }
 
 echo "Starting server as web"
 start_web
-
-
